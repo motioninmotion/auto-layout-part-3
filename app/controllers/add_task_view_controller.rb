652ef64608f9a843ceec54d39149be9b6277bcec
Jack Dean Watson-Hamblin <info@fluffyjack.com>
2014-11-18 23:00:39 +1100

    Episode 5 of RubyMotion for Rails Developer complete - Auto-layout basics

--- app/controllers/add_task_view_controller.rb
+++ app/controllers/add_task_view_controller.rb
@@ -2,5 +2,7 @@ class AddTaskViewController < UIViewController
   def loadView
     self.title = "Add Task"
     self.view = AddTaskView.new
+    view.layout_guide = self.topLayoutGuide
+    view.layout_views
   end
 end
--- app/views/add_task_view.rb
+++ app/views/add_task_view.rb
@@ -1,4 +1,83 @@
 class AddTaskView < UIView
+  attr_accessor :layout_guide
+
+  def layout_views
+    @title_label.translatesAutoresizingMaskIntoConstraints = false
+    @notes_label.translatesAutoresizingMaskIntoConstraints = false
+
+    addConstraint(
+      NSLayoutConstraint.constraintWithItem(
+        @title_label,
+        attribute: NSLayoutAttributeTop,
+        relatedBy: NSLayoutRelationEqual,
+        toItem: layout_guide,
+        attribute: NSLayoutAttributeBottom,
+        multiplier: 1.0,
+        constant: 20
+      )
+    )
+
+    addConstraint(
+      NSLayoutConstraint.constraintWithItem(
+        @title_label,
+        attribute: NSLayoutAttributeLeft,
+        relatedBy: NSLayoutRelationEqual,
+        toItem: self,
+        attribute: NSLayoutAttributeLeft,
+        multiplier: 1.0,
+        constant: 20
+      )
+    )
+
+    addConstraint(
+      NSLayoutConstraint.constraintWithItem(
+        @title_label,
+        attribute: NSLayoutAttributeRight,
+        relatedBy: NSLayoutRelationEqual,
+        toItem: self,
+        attribute: NSLayoutAttributeRight,
+        multiplier: 1.0,
+        constant: -20
+      )
+    )
+
+    addConstraint(
+      NSLayoutConstraint.constraintWithItem(
+        @notes_label,
+        attribute: NSLayoutAttributeTop,
+        relatedBy: NSLayoutRelationEqual,
+        toItem: @title_label,
+        attribute: NSLayoutAttributeBottom,
+        multiplier: 1.0,
+        constant: 20
+      )
+    )
+
+    addConstraint(
+      NSLayoutConstraint.constraintWithItem(
+        @notes_label,
+        attribute: NSLayoutAttributeLeft,
+        relatedBy: NSLayoutRelationEqual,
+        toItem: self,
+        attribute: NSLayoutAttributeLeft,
+        multiplier: 1.0,
+        constant: 20
+      )
+    )
+
+    addConstraint(
+      NSLayoutConstraint.constraintWithItem(
+        @notes_label,
+        attribute: NSLayoutAttributeRight,
+        relatedBy: NSLayoutRelationEqual,
+        toItem: self,
+        attribute: NSLayoutAttributeRight,
+        multiplier: 1.0,
+        constant: -20
+      )
+    )
+  end
+
   def init
     super
 
